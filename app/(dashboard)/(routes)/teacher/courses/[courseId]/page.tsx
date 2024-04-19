701eb91da2743fac63bbfd28d90784c36a1b64ae
Okware-L <lewisokware@gmail.com>
2024-04-19 19:38:42 +0300

    uploadthing-up

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,9 @@ import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
+import { ImageForm } from "./_components/image-form";
+
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -51,6 +54,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             <h2 className="text-xl">Customize your course</h2>
           </div>
           <TitleForm initialData={course} courseId={course.id} />
+
+          <DescriptionForm initialData={course} courseId={course.id} />
+
+          <ImageForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
