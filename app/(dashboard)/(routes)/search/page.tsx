10c4761e1c6820ed40314b5921914f98dc14a08d
Okware-L <lewisokware@gmail.com>
2024-04-19 10:30:22 +0300

    first commit

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchPage = () => {
+  return <div> searchpage</div>;
+};
+
+export default SearchPage;
---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const logo = () => {
+  return <Image height={60} width={50} alt="logo" src="jmlogoblack.svg" />;
+};
+
+export default logo;
